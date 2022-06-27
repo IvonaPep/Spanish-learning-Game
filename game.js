d9269e56de910fa344a987821cea38ff7a63a592
Ivona Pepic <ivona.pepic@gmail.com>
2022-06-27 18:29:39 +0200

    solved level 1 problems + started level 2

--- game.js
+++ game.js
@@ -1,86 +1,98 @@
+// import animalsArr from "./data";
+// console.log(animalsArr);
+
 const animalsArr = [
   {
     title: "gato",
-    question:
-      "https://cdn-icons-png.flaticon.com/512/1864/1864514.png",
+    question: "https://cdn-icons-png.flaticon.com/512/1864/1864514.png",
     answers: ["pato", "gato", "oso"],
   },
   {
     title: "perro",
-    question:
-      "https://cdn-icons-png.flaticon.com/512/1998/1998627.png",
+    question: "https://cdn-icons-png.flaticon.com/512/1998/1998627.png",
     answers: ["perro", "pato", "gato"],
   },
   {
     title: "leon",
-    question:
-      "https://cdn-icons-png.flaticon.com/512/1998/1998713.png",
+    question: "https://cdn-icons-png.flaticon.com/512/1998/1998713.png",
     answers: ["gato", "leon", "oso"],
   },
-  { title: "vaca",
+  {
+    title: "vaca",
     question: "https://cdn-icons-png.flaticon.com/512/1998/1998610.png",
     answers: ["vaca", "leon", "oso"],
   },
-  { title: "cerdo",
-  question:
-  "https://cdn-icons-png.flaticon.com/512/1998/1998749.png",
-  answers: ["oso", "leon", "cerdo"],
+  {
+    title: "cerdo",
+    question: "https://cdn-icons-png.flaticon.com/512/1998/1998749.png",
+    answers: ["oso", "leon", "cerdo"],
   },
-  { title: "oveja",
-  question: "https://cdn-icons.flaticon.com/png/512/2711/premium/2711858.png?token=exp=1656319435~hmac=bd55b79f602e6b15f9af6fcaab970407",
-  answers: ["oveja", "gato", "oso"],
+  {
+    title: "oveja",
+    question:
+      "https://cdn-icons.flaticon.com/png/512/2711/premium/2711858.png?token=exp=1656319435~hmac=bd55b79f602e6b15f9af6fcaab970407",
+    answers: ["oveja", "gato", "oso"],
   },
-  { title: "pajaro",
-  question: "https://cdn-icons.flaticon.com/png/512/2677/premium/2677104.png?token=exp=1656325868~hmac=a11ba7c281e3d5beb7da534f510af496",
-  answers: ["cerdo", "leon", "pajaro"],
+  {
+    title: "pajaro",
+    question:
+      "https://cdn-icons.flaticon.com/png/512/2677/premium/2677104.png?token=exp=1656325868~hmac=a11ba7c281e3d5beb7da534f510af496",
+    answers: ["cerdo", "leon", "pajaro"],
   },
-  { title: "burro",
-  question:"https://cdn-icons-png.flaticon.com/128/3802/3802054.png",
-  answers: ["oso", "oveja", "burro"],
+  {
+    title: "burro",
+    question: "https://cdn-icons-png.flaticon.com/128/3802/3802054.png",
+    answers: ["oso", "oveja", "burro"],
   },
-  { title: "caballo",
-    question : "https://cdn-icons-png.flaticon.com/512/1998/1998679.png",
+  {
+    title: "caballo",
+    question: "https://cdn-icons-png.flaticon.com/512/1998/1998679.png",
     answers: ["leon", "caballo", "pajaro"],
   },
-  { title: "conejo",
+  {
+    title: "conejo",
     question: "https://cdn-icons-png.flaticon.com/512/1998/1998765.png",
     answers: ["conejo", "gato", "cerdo"],
   },
-  { title: "cabra",
+  {
+    title: "cabra",
     question: "https://cdn-icons-png.flaticon.com/128/1998/1998662.png",
     answers: ["oso", "cabra", "pajaro"],
   },
-  { title: "pato",
+  {
+    title: "pato",
     question: "https://cdn-icons-png.flaticon.com/128/1012/1012677.png",
     answers: ["pato", "cabra", "conejo"],
-},
+  },
 ];
-
 const startBtn = document.getElementById("start-button");
 startBtn.addEventListener("click", startGame);
-
-let points  = 0;
 
 const questionImage = document.getElementById("image-question");
 const answerButtons = document.getElementById("answer-buttons");
 
+const levelBtn1 = document.getElementById("level-btn1");
+levelBtn1.addEventListener("click", startGame);
+
+const levelBtn2 = document.getElementById("level-btn2");
+levelBtn2.addEventListener("click", startLevel2);
+
 const random = Math.floor(Math.random() * animalsArr.length);
+let takeAQuestion = "";
 
 function startGame() {
-  
-
+  console.log("started");
+  levelBtn1.style.visibility = "hidden";
+  levelBtn2.style.visibility = "hidden";
   startBtn.style.visibility = "hidden";
   takeAQuestion = getRandomQuest();
   displayQuestion(takeAQuestion);
-  chooseAnswer();
 }
-
 
 function getRandomQuest() {
   const random = Math.floor(Math.random() * animalsArr.length);
   return animalsArr[random];
 }
-
 
 function displayQuestion(randomQuest) {
   document.getElementById("answer-btn1").innerHTML = randomQuest.answers[0];
@@ -93,15 +105,55 @@ function displayQuestion(randomQuest) {
   document.getElementById("points-sec").style.visibility = "visible";
 }
 
+let points = 0;
 
- let newHandle = function chooseAnswer (ev) {
-    if (ev.target.innerText === takeAQuestion.title) {
-       points++;
-       (document.getElementById("points").innerHTML) = points;
-      startGame();
-    }
-  };
- 
-  answerButtons.addEventListener("click", newHandle);
+function chooseAnswer(ev) {
+  if (ev.target.innerText === takeAQuestion.title) {
+    points++;
+    document.getElementById("points").innerHTML = points;
+    startGame();
+  }
+}
+
+answerButtons.addEventListener("click", (e) => chooseAnswer(e));
+
+// **********************************************************************************
+// level 2
+let input;
+
+function startLevel2() {
+  console.log("started");
+  levelBtn1.style.visibility = "hidden";
+  levelBtn2.style.visibility = "hidden";
+  startBtn.style.visibility = "hidden";
+  takeAQuestion = getRandomQuest();
+  displayQuestion2(takeAQuestion);
+  //chooseAnswer(takeAQuestion);
+}
+
+function displayQuestion2(randomQuest) {
+  document.getElementById("answer-buttons").style.visibility = "hidden";
+  document.getElementById("image-question").src = randomQuest.question;
+  document.getElementById("question-images").style.visibility = "visible";
+  document.getElementById("points-sec").style.visibility = "visible";
+
+  input = document.createElement("input");
+  input.type = "text";
+  input.placeholder = "?";
+  input.className = "answer-input";
+  answerButtons.appendChild(input);
+  input.style.visibility = "visible";
+  input.value ="";
+}
+
+function chooseAnswer2() {
+  
+  if (input.value.toLowercase() === takeAQuestion.title) {
+    points++;
+    document.getElementById("points").innerHTML = points;
+    startLevel2();
+  }
+}
+// input.addEventListener("input", chooseAnswer2(e));
 
 
